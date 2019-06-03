48223ab6e430f61dfb9309b0bb51196f3a60ebf6
Wayne Doyle <12053104+wayneidoyle@users.noreply.github.com>
2019-06-03 15:21:14 -0700

    Restricted percentage reporting

--- singlecellfusion/helpers.py
+++ singlecellfusion/helpers.py
@@ -1304,11 +1304,11 @@ def constrained_one_direction(output_loom,
         # Get a random selection of cells
         np.random.shuffle(rejected_cells)
         if verbose:
-            base_rej = '{0} ({1}%) cells need to find neighbors'
+            base_rej = '{0} ({1:.2f}%) cells need to find neighbors'
             help_log.info(base_rej.format(rejected_cells.size,
                                           (
                                                       rejected_cells.size / self_num) * 100))
-            base_sat = '{0} ({1}%) cells can make neighbors'
+            base_sat = '{0} ({1:.2f}%) cells can make neighbors'
             help_log.info(base_sat.format(unsaturated_cells.size,
                                           (
                                                       unsaturated_cells.size / other_num) * 100))
